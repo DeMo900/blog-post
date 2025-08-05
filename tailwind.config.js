a20e003a8532b078626b091564082ec00c8fc435
Eli September <eli.september08@gmail.com>
2025-08-05 16:03:08 +0200

    Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",
+    "./assets/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
