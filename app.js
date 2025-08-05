be9de463a0c29ca8a53ff958120af97be9842b14
DeMo900 <dodoadam893@gmail.com>
2025-08-05 17:38:10 +0300

    basics

---
+++ app.js
@@ -0,0 +1,22 @@
+
+//basics packages
+const express = require("express")
+const mongo = require("mongoose")
+const app = express()
+const path = require("path")
+//////
+app.set("view engine","ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"assets")))
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(`error while starting ${err}`)
+    }else{
+        console.log("conected to the server")
+    }
+})
+app.get("/",(req,res)=>{
+    res.render(path.join(__dirname,"views","tailwind_test.ejs"))
+})
