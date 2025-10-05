0329985f824f7fc16d7bbcc19faa2efb6e257f4b
Eli September <eli.september08@gmail.com>
2025-10-05 11:27:06 +0200

    added popups to display errors

--- assets/js/common.js
+++ assets/js/common.js
@@ -32,3 +32,46 @@ function toggleHidden(e, afterFade = false, afterFadeTimer = 200) {
         }
     }
 }
+
+let popups = document.getElementById('popups');
+
+let popupTemp = `
+<div id="popupModalContainer" class="z-10">
+    <div id="popupModal">
+        <h2 id="popupModalTitle">Type</h2>
+        <p id="popupModalDescription">Description...</p>
+        <button id="popupModalClose" onclick="closePopup()">Close</button>
+    </div>
+</div>
+`
+
+function closePopup() {
+    popups.innerHTML = '';
+    toggleHidden(popups);
+}
+
+function showPopup(type, details) {
+    popups.innerHTML = popupTemp;
+    let popupModal = document.getElementById('popupModal');
+    let popupModalTitle = document.getElementById('popupModalTitle');
+    let popupModalDescription = document.getElementById('popupModalDescription');
+    
+    popupModalTitle.textContent = type.toUpperCase();
+    popupModalDescription.textContent = details;
+    
+    toggleHidden(popups);
+    
+    switch (type) {
+        case 'info':
+            popupModal.classList.add('info');
+            break;
+        case 'warning':
+            popupModal.classList.add('warning');
+            break;
+        case 'error':
+            popupModal.classList.add('danger');
+            break;
+        default:
+            closePopup();
+    }
+}
